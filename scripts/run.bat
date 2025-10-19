40742d4efacd32039248068f0ca6e09674824882
mujukno1-ship-it <mujukno1@gmail.com>
2025-10-19 18:58:41 +0900

    Delete scripts directory

--- scripts/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM ===== 쩔어지갑 서버 실행 (폴더 자동 맞춤) =====
-cd /d "%~dp0"
-cd ..
-set PORT=8080
-start "" http://localhost:%PORT%/index.html
-python -m http.server %PORT%
-pause
--- scripts/run.ps1
+++
@@ -1,6 +0,0 @@
-
-param(
-  [int]$Port = 8080
-)
-Write-Host "Running local server on http://localhost:$Port"
-python -m http.server $Port -d .
