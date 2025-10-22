797828b3ba938e8466a6b006dc73e7161a7eb048
mujukno1-ship-it <mujukno1@gmail.com>
2025-10-22 21:04:38 +0900

    Update upbit.js

--- api/upbit.js
+++ api/upbit.js
@@ -1,14 +1,11 @@
 export default async function handler(req, res) {
   const target = req.query.url;
   try {
-    const response = await fetch(target);
-    const text = await response.text();
-
-    // 브라우저 접근 허용
+    const r = await fetch(target);
+    const text = await r.text();
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
     res.status(200).send(text);
   } catch (err) {
     res.status(500).json({ error: err.message });
