3b1027a0ba9a90fc5866a88d433b07a709427ed8
mujukno1-ship-it <mujukno1@gmail.com>
2025-10-24 21:10:17 +0900

    Create onchain.js

---
+++ functions/api/onchain.js
@@ -0,0 +1,13 @@
+export const onRequestGet = async () => {
+  try {
+    const stables = await fetch("https://stablecoins.llama.fi/summary").then(r=>r.json());
+    return new Response(JSON.stringify({ ok:true, stables }), {
+      headers: { "content-type":"application/json; charset=utf-8", "access-control-allow-origin":"*" }
+    });
+  } catch (e) {
+    return new Response(JSON.stringify({ ok:false, error: String(e) }), {
+      status: 500,
+      headers: { "content-type":"application/json; charset=utf-8", "access-control-allow-origin":"*" }
+    });
+  }
+};
